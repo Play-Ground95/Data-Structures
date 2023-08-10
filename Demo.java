df9037b4d6842eec17662f27d446c0356bd6be74
Kavindu95 <kavindueng95@gmail.com>
2023-08-10 22:43:44 +0530

    fixed-arrays

---
+++ Demo.java
@@ -0,0 +1,32 @@
+public class Demo{
+    public static void main(String[] args) {
+        //Fixed / static arrays --> have building support(built-in the language)
+        //Dynamic arrays (java se library)
+
+        String name1= "kasun";
+        String name2= "amal";
+        String name3= "nimal";
+        String name4= "supun";
+        String name5= "kavindu";
+
+        //System.out.println(name1); can print 5 names using sout
+
+        /*
+         new Datatype [size]
+
+            -new int[5]
+
+            -new char[10]
+
+            -new long[0]
+         */
+
+        String [] names=new String[5];
+        names[0]="kasun";
+        names[1]= "amal";
+        names[2]= "nimal";
+        names[3]= "supun";
+        names[4]= "kavindu";
+        
+    }
+}
---
+++ Demo4.java
@@ -0,0 +1,18 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        
+        int x=10;
+        int y;
+        long myLong=20;
+
+        int[] myArray=new int[5];
+        int[] myArray2=myArray;
+        String something="IJSE";
+
+        System.out.println(something);
+        System.out.println(something.toString()); //by default it get string value
+        //System.out.println(myArray);
+        
+    }
+    
+}
